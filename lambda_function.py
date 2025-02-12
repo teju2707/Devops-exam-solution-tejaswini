0abc194c0064e5a0aca5e61a4b2012728b0fde18
Tejaswini Wakte <tejaswiniprasadwakte@gmail.com>
2025-02-12 17:27:10 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,25 +1,19 @@
 import json
 import requests
-import os
 
 def lambda_handler(event, context):
-    subnet_id = os.environ['SUBNET_ID']
-    name = os.environ['NAME']
-    email = os.environ['EMAIL']
-
-    payload = {
-        "subnet_id": subnet_id,
-        "name": name,
-        "email": email
-    }
-
+    url = "https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data"
     headers = {
-        'Content-Type': 'application/json',
-        'X-Siemens-Auth': 'test'
+        'X-Siemens-Auth': 'test',
+        'Content-Type': 'application/json'
     }
-
-    response = requests.post('https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data', headers=headers, data=json.dumps(payload))
+    payload = {
+        "subnet_id": event["subnet_id"],
+        "name": event["name"],
+        "email": event["email"]
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
     return {
         'statusCode': response.status_code,
-        'body': response.json()
+        'body': response.text
     }
